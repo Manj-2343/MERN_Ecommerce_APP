726aff1625b36e8ef35f23311189e9db45530646
Manoj Biswal <manojbiswal521@gmail.com>
2024-09-13 05:09:38 +0530

    add new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import AuthLogin from "./pages/auth/Login";
+import AuthRegister from "./pages/auth/Register";
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/Layout";
 
 const App = () => {
-  return <div>My react code</div>;
+  return (
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
